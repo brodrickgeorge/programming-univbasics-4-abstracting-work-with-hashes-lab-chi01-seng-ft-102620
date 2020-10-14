bcd1e596e677c192c08803d098dab5b8d61f70d3
Brodrick George <brodrick.george@yahoo.com>
2020-10-14 22:40:38 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -4,7 +4,7 @@ def my_hash_creator(key, value)
   end
 
 def read_from_hash(hash, key)
- {hash => key}
+ 
 
 def update_counting_hash(hash, key)
   # given a hash an a key as parameters, return an updated hash
